9ec13f1786203a6db3ad79b82ef2ae133935a87b
Nour <nourbahloul.192002@gmail.com>
2025-09-28 22:10:46 +0100

    clean dentiste route

--- backend/routes/dentisteRoutes.js
+++ backend/routes/dentisteRoutes.js
@@ -1,11 +1,9 @@
 import express from "express";
 
 import { createPatient, deletePatient, getPatients,updatePatient } from "../controllers/patientController.js";
-import { createRendezVous, deleteRendezVous, getQuickStats, getRendezVous, getRendezVousAnnual, getRendezVousCourbe, getRendezVousHistogramme, updateRendezVous } from "../controllers/RendezVousController.js";
 import { createSecretaire, deleteSecretaire, getSecretaires,  updateSecretaire } from "../controllers/SecretaireController.js";
 import { authenticateToken } from "./AuthRoutes.js";
 const router = express.Router();
-import Dentiste from '../models/Dentiste.js';
 
 // --- Middlewares ---
 const requireDentiste = (req, res, next) => {
@@ -18,25 +16,11 @@ const requireDentiste = (req, res, next) => {
 
 
 
-export const attachDentiste = async (req, res, next) => {
-    try {
-        if (req.user?.role !== 'dentiste') {
-            return res.status(403).json({ error: 'Accès dentiste requis' });
-        }
-        const dentiste = await Dentiste.findOne({ where: { userId: req.user.userId } });
-        if (!dentiste) return res.status(404).json({ error: 'Profil dentiste introuvable' });
-        req.dentiste = dentiste;
-        next();
-    } catch (e) {
-        next(e);
-    }
-};
 
 
 
 router.use(authenticateToken);
 router.use(requireDentiste);
-router.use(attachDentiste); // ✅ maintenant req.dentiste existe
 
 
 router.post("/patients", createPatient);
@@ -55,16 +39,7 @@ router.delete("/secretaires/:id", deleteSecretaire);
 
 
 
-router.get("/rendezvous", getRendezVous);
-router.post("/rendezvous", createRendezVous);
-router.put("/rendezvous/:id", updateRendezVous);
-router.delete("/rendezvous/:id", deleteRendezVous);
 
-
-router.get("/stats/histogramme", getRendezVousHistogramme);
-router.get("/stats/courbe", getRendezVousCourbe);
-router.get("/stats/quick", getQuickStats);
-router.get("/stats/annual", getRendezVousAnnual);
 
 
 export default router;
